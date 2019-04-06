5f77b1dab9e1aa69ce46a51777771eb627c22cdd
Xuwei98 <23023918+Xuwei98@users.noreply.github.com>
2019-04-06 23:06:44 +0900

    Site updated: 2019-04-06 23:06:18

---
+++ uploads/MFC/rename.py
@@ -0,0 +1,13 @@
+import os
+# 列出当前目录下所有的文件
+files = os.listdir(".")      
+
+for filename in files:
+    portion = os.path.splitext(filename)
+    # 如果后缀是.txt
+    if portion[1] == ".PNG": 
+        # 重新组合文件名和后缀名  
+        newname = portion[0] + ".jpg"  
+        os.rename(filename,newname)
+
+ 
