4bb118707044100155ffd183a4a4c0edb28eceb4
Xuwei98 <23023918+Xuwei98@users.noreply.github.com>
2019-04-07 18:13:38 +0900

    Site updated: 2019-04-07 18:13:15

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    volume: 0.4,
+    audio: [{
+            name: 'For you',
+            artist: 'Androp',
+            url: 'http://m10.music.126.net/20190407171018/82a40852f62482122c5b91a2d7cc8d11/ymusic/555a/515c/5459/b0df4da055f622276867c344934b70b8.mp3',
+            cover: 'http://www.littleoslo.com/lyj/home/wp-content/uploads/2018/11/koi-450x450.jpg'
+        }]
+});
