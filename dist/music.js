d3d2c8565588687723b75ca758528b25377e9495
Xuwei98 <23023918+Xuwei98@users.noreply.github.com>
2019-04-07 18:25:12 +0900

    Site updated: 2019-04-07 18:25:09

--- dist/music.js
+++ dist/music.js
@@ -5,7 +5,7 @@ const ap = new APlayer({
     audio: [{
             name: 'For you',
             artist: 'Androp',
-            url: 'http://m10.music.126.net/20190407171018/82a40852f62482122c5b91a2d7cc8d11/ymusic/555a/515c/5459/b0df4da055f622276867c344934b70b8.mp3',
+            url: 'https://link.hhtjim.com/163/1347159126.mp3',
             cover: 'http://www.littleoslo.com/lyj/home/wp-content/uploads/2018/11/koi-450x450.jpg'
         }]
 });
